8aa00a0f3b85c33b9d8224a013150aa8a1527aa9
Tarunav BA <tarunavba@gmail.com>
2021-08-29 22:26:22 +0530

    👌 IMPROVE: Contact-Book.py

--- Contact-Book.py
+++ Contact-Book.py
@@ -1,7 +1,18 @@
 print("Welcome to Contact Book! :)") 
 
+my_array = []
+
 save = input("If you want to save a contact enter save: ")
 
 if save == "save":
     name = input("Enter the name of the person: ")
     number = input("Enter the phone number of the person: ")
+    my_array.append(name)
+    my_array.append(number)
+    print("Successfully saved the contact! :)")
+
+see = input("If you want to take a look at the saved contacts, enter see: ")
+
+if see == "see":
+    for i in my_array:
+        print(i)
