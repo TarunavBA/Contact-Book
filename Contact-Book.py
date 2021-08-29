27843179ee96e9695f010e660218a75397a59b18
Tarunav BA <tarunavba@gmail.com>
2021-08-29 22:15:02 +0530

    👌 IMPROVE: Contact-Book

--- Contact-Book.py
+++ Contact-Book.py
@@ -3,5 +3,5 @@ print("Welcome to Contact Book! :)")
 save = input("If you want to save a contact enter save: ")
 
 if save == "save":
-    input("Enter the name of the person: ")
-    input("Enter the phone number of the person: ")
+    name = input("Enter the name of the person: ")
+    number = input("Enter the phone number of the person: ")
